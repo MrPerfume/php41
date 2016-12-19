d8ce2c81cf525570fa87fbe6440737ab0e3ba93d
MrPerfume <hoangluckyboy@icloud.com>
2016-12-19 15:28:26 +0800

    记得把target属性设置下

--- shop/Runtime/Cache/Back/de1109263561ab99c8a777d354ba4bf8.php
+++ shop/Runtime/Cache/Back/de1109263561ab99c8a777d354ba4bf8.php
@@ -108,7 +108,7 @@
                                                            src="<?php echo (C("BACK_IMG_URL")); ?>menu_icon.gif" width=9></td>
                             <td><a class=menuchild 
                                    href="<?php echo U('Goods/showlist');?>"
-                                   target=main>产品展示</a></td></tr>
+                                   target=right>产品展示</a></td></tr>
                         <tr height=20>
                             <td align=middle width=30><img height=9 
                                                            src="<?php echo (C("BACK_IMG_URL")); ?>menu_icon.gif" width=9></td>
