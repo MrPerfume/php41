40f8f35ee57caefe8a9a2acf812e4b2b9198d601
MrPerfume <hoangluckyboy@icloud.com>
2016-12-19 14:25:48 +0800

    后台登录页面的完善

--- shop/Common/Conf/config.php
+++ shop/Common/Conf/config.php
@@ -1,4 +1,12 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+    //前台
+    'CSS_URL' => '/Home/Public/style/',
+    'JS_URL' => '/Home/Public/js/',
+    'IMG_URL' => '/Home/Public/images/',
+    //后台
+    'BACK_CSS_URL' => '/Back/Public/css/',
+    'BACK_JS_URL' => '/Back/Public/js/',
+    'BACK_IMG_URL' => '/Back/Public/img/',
 );
--- shop/Home/Conf/config.php
+++ shop/Home/Conf/config.php
@@ -2,8 +2,4 @@
 return array(
 	//'配置项'=>'配置值'
 
-    //给项目做静态文件访问路劲的配置
-    'CSS_URL'=>'/Home/Public/style/',
-    'JS_URL'=>'/Home/Public/js/',
-    'IMG_URL'=>'/Home/Public/images/',
 );
