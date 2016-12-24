203ab7cac5cca97db72b79dd2347f0d9919aa0e2
MrHuang <hoangluckyboy@icloud.com>
2016-12-24 23:56:15 +0800

    使用验证码类

--- shop/Back/Controller/AdminController.class.php
+++ shop/Back/Controller/AdminController.class.php
@@ -5,4 +5,19 @@ class AdminController extends Controller {
     public function index(){
         $this->display();
     }
+
+
+    public function verifyImg(){
+        //显示验证码
+        $cfg = array(
+            'imageH'    =>  40,               // 验证码图片高度
+            'imageW'    =>  100,               // 验证码图片宽度
+            'length'    =>  4,               // 验证码位数
+            'fontttf'   =>  '4.ttf',              // 验证码字体，不设置随机获取
+            'fontSize'  =>  15,              // 验证码字体大小(px)
+        );
+        $very = new \Think\Verify($cfg);
+        $very -> entry();
+    }
+
 }
