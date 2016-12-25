14aba9e2724898186bedc78a01891299ace66e74
MrHuang <hoangluckyboy@icloud.com>
2016-12-25 22:08:32 +0800

    登陆
    session持久化 登入登出已完成

--- shop/Back/Controller/AdminController.class.php
+++ shop/Back/Controller/AdminController.class.php
@@ -1,17 +1,47 @@
 <?php
 namespace Back\Controller;
 use Think\Controller;
+
 class AdminController extends Controller {
-    public function index(){
-        $this->display();
+    public function login(){
+        if(IS_POST){
+          //  dump($_POST);
+           // exit;
+            //array(3) {
+            //  ["admin_user"] => string(3) "tom"
+            //  ["admin_psd"] => string(3) "123"
+            //  ["captcha"] => string(4) "ffjb"
+            //}
+            $name = $_POST['admin_user'];
+            $pwd = $_POST['admin_psd'];
+            //用户名和密码的校验
+            $manager = D('Manager');
+            $info = $manager ->where(array('mg_name'=>$name,'mg_pwd'=>$pwd))-> find();
+            if($info != null){
+                //session持久化操作（id/name）
+                session('admin_id',$info['mg_id']);
+                session('admin_name',$info['mg_name']);
+                //页面跳转
+                $this -> redirect('Index/index');
+            }
+            $this -> error('用户名或密码错误',U('login'),2);
+        }else{
+            $this -> display();
+        }
     }
 
+    //管理员退出系统
+    public function logout(){
+        session(null); //清空session
+        //$this -> redirect('[分组/控制器/]操作方法');
+        $this -> redirect('login'); //页面跳转到登录页
+    }
 
     public function verifyImg(){
         //显示验证码
         $cfg = array(
             'imageH'    =>  40,               // 验证码图片高度
-            'imageW'    =>  100,               // 验证码图片宽度
+            'imageW'    =>  120,               // 验证码图片宽度
             'length'    =>  4,               // 验证码位数
             'fontttf'   =>  '4.ttf',              // 验证码字体，不设置随机获取
             'fontSize'  =>  15,              // 验证码字体大小(px)
@@ -20,4 +50,14 @@ class AdminController extends Controller {
         $very -> entry();
     }
 
+    //ajax过来校验验证码
+    function checkCode(){
+        $code = I('get.code'); //获得用户输入的验证码
+        $vry = new \Think\Verify();
+        if($vry -> check($code)){
+            echo json_encode(array('status'=>1));
+        }else{
+            echo json_encode(array('status'=>2));
+        }
+    }
 }
--- shop/Back/Controller/IndexController.class.php
+++ shop/Back/Controller/IndexController.class.php
@@ -3,6 +3,11 @@ namespace Back\Controller;
 use Think\Controller;
 
 class IndexController extends Controller {
+    function __construct(){
+        parent::__construct();//先执行父类构造方法
+        layout(false); // 临时关闭当前模板的布局功能
+    }
+
     public function head(){
         $this -> display();
     }
