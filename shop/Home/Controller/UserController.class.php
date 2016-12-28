686fbd75623d058b7d1265498eb438775a7ec808
MrHuang <hoangluckyboy@icloud.com>
2016-12-28 23:52:40 +0800

    前台用户的注册
    注册使用的验证码和后台一样

--- shop/Home/Controller/UserController.class.php
+++ shop/Home/Controller/UserController.class.php
@@ -2,6 +2,32 @@
 namespace Home\Controller;
 use Think\Controller;
 class UserController extends Controller {
+    function verifyImg(){
+        //显示验证码
+        $cfg = array(
+            'imageH'    =>  40,               // 验证码图片高度
+            'imageW'    =>  100,               // 验证码图片宽度
+            'length'    =>  4,               // 验证码位数
+            'fontttf'   =>  '4.ttf',              // 验证码字体，不设置随机获取
+            'fontSize'  =>  15,              // 验证码字体大小(px)
+        );
+        $very = new \Think\Verify($cfg);
+        $very -> entry();
+    }
+
+
+    //ajax过来校验验证码
+    function checkCode(){
+        $code = I('get.code'); //获得用户输入的验证码
+        $vry = new \Think\Verify();
+        if($vry -> check($code)){
+            echo json_encode(array('status'=>1));
+        }else{
+            echo json_encode(array('status'=>2));
+        }
+    }
+
+
     public function login(){
         $this->display();
     }
