3ce63ee4409a2a0e7b05a6ba3ad6831d0b2d3750
MrHuang <hoangluckyboy@icloud.com>
2016-12-29 23:54:15 +0800

    添加邮箱注册功能
    学会使用插件

--- shop/Common/Common/function.php
+++ shop/Common/Common/function.php
@@ -17,3 +17,31 @@ function fanXSS($string) {
     // 过滤字符串
     return $obj->purify($string);
 }
+
+//发送邮件
+function sendMail($to, $title, $content){
+    require_once('./Plugin/phpmailer/class.phpmailer.php');
+    $mail = new PHPMailer();
+    // 设置为要发邮件
+    $mail->IsSMTP();
+    // 是否允许发送HTML代码做为邮件的内容
+    $mail->IsHTML(TRUE);
+    $mail->CharSet='UTF-8';
+    // 是否需要身份验证
+    $mail->SMTPAuth=TRUE;
+    /*  邮件服务器上的账号是什么 -> 到163注册一个账号即可 */
+    $mail->From="huang2lan@163.com";
+    $mail->FromName="MrHuang";
+    $mail->Host="smtp.163.com";  //发送邮件的服务协议地址
+    $mail->Username="huang2lan";
+    $mail->Password="doufu2maomao";
+    // 发邮件端口号默认25
+    $mail->Port = 25;
+    // 收件人
+    $mail->AddAddress($to);
+    // 邮件标题
+    $mail->Subject=$title;
+    // 邮件内容
+    $mail->Body=$content;
+    return($mail->Send());
+}
